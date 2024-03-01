51e15b82a21410a71dbaea8523613d6300ad0c0c
chandrasoumya <121810090+chandrasoumya@users.noreply.github.com>
2024-03-01 12:25:09 +0000

    outlook to gmail

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,7 +5,7 @@ const Contact =()=>{
         <div className="contact">             
                 <a href="https://www.linkedin.com/in/soumyadeep-chandra-9a8a4a27b" className="linkedin"><img  src="/photos/logo-linkedin.png" alt="loading"/></a>
                 <a href="https://wa.me/+916297655144" className="whatsapp"><img src="/photos/logo-whatsapp.png" alt="loading"/></a>   
-                <a href="maiilto:Soumyadeep.Chandra2022@iem.edu.in" className="outlook"><img src="/photos/logo-outlook.png" alt="loading" id="outlook-logo"/> </a>
+                <a href="mailto:soumya220033@gmail.com" className="gmail"><img src="/photos/logo-gmail.png" alt="loading" id="outlook-logo"/> </a>
         </div>
     )
 }
